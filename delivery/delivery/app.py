f7ba646c70bfe7020ea8341833eae2b34ee81270
es99 <engels.franca@gmail.com>
2020-08-07 01:17:05 -0300

    Make app installable

--- delivery/delivery/app.py
+++ delivery/delivery/app.py
@@ -1,7 +1,8 @@
-import views
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    views.init_app(app)
     return app
+
+if __name__ == "__main__":
+    print("Eu quero dar uma sentada")
