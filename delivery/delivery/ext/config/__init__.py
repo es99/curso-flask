fa75d7822defc4236a435d844e6d1d31af6dcd7a
es99 <engels.franca@gmail.com>
2020-08-12 02:41:53 -0300

    alterando config

--- delivery/delivery/ext/config/__init__.py
+++ delivery/delivery/ext/config/__init__.py
@@ -1,5 +1,7 @@
 def init_app(app):
     app.config["SECRET_KEY"] = "engelsink666"
+    app.config["ENV"] = "development"
+    app.config["DEBUG"] = True
 
     if app.debug:
         app.config["DEBUG_TB_TEMPLATE_EDITOR_ENABLED"] = True
