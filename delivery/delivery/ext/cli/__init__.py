03446ccc89894a2c4f2d1c14d0040e80d2226d9e
es99 <engels.franca@gmail.com>
2020-08-12 10:29:03 -0300

    SQLAlchemy e modelagem de banco de dados

--- delivery/delivery/ext/cli/__init__.py
+++ delivery/delivery/ext/cli/__init__.py
@@ -1,5 +1,6 @@
 import click
 from delivery.ext.db import db
+from delivery.ext.db import models
 
 def init_app(app):
     @app.cli.command()
@@ -7,6 +8,21 @@ def init_app(app):
         """Este comando inicializa o db"""
         db.create_all()
 
+    @app.cli.command()
+    @click.option("--email", "-e")
+    @click.option("--passwd", "-p")
+    @click.option("--admin", "-a", is_flag=True, default=False)
+    def add_user(email, passwd , admin):
+        """adiciona novo usuário"""
+        user = models.User(
+            email=email,
+            passwd=passwd,
+            admin=admin
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        click.echo(f"Usuário {email} criado com sucesso")
 
     @app.cli.command()
     def listar_pedidos():
